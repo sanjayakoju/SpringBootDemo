6d8ae0fdae2183d8e55df1de250ae22e99b16861
Sanjaya Koju <40861550+sanjayakoju@users.noreply.github.com>
2022-08-03 22:09:14 -0500

    Spring Rest, internationalization i18N setup

--- src/main/java/com/ea/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/ea/springbootdemo/SpringBootDemoApplication.java
@@ -2,6 +2,13 @@ package com.ea.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
@@ -9,5 +16,28 @@ public class SpringBootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
+
+//    @Bean
+//    public LocaleResolver localeResolver() {
+//        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+//        localeResolver.setDefaultLocale(Locale.US);
+//        return localeResolver;
+//    }
+
+    // Simplified of above method
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    // This method is configuration using application properties
+//    @Bean
+//    public ResourceBundleMessageSource resourceBundleMessageSource() {
+//        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+//        resourceBundleMessageSource.setBasename("messages");
+//        return resourceBundleMessageSource;
+//    }
 
 }
--- src/main/java/com/ea/springbootdemo/controller/HelloController.java
+++ src/main/java/com/ea/springbootdemo/controller/HelloController.java
@@ -1,13 +1,31 @@
 package com.ea.springbootdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    ResourceBundleMessageSource messageSource;
+
+//    @GetMapping(path = "/hello")
+//    public String getHello(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+//        return messageSource.getMessage("good.morning.message", null, locale);
+//    }
+
+    // Simplified of above method
     @GetMapping(path = "/hello")
     public String getHello() {
-        return "Hello World !!";
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+    }
+
+    @GetMapping(path = "/hello/{name}")
+    public String getHello(@PathVariable String name) {
+        return "Hello "+name;
     }
 }
